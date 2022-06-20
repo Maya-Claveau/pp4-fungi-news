353a9672776bb0ed64ad2fee08b75f386ec18c0b
Maya-Claveau <maya.claveau@gmail.com>
2022-06-20 12:21:34 +0000

    Add code to View.py and link urls, add templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """
+    a post list based on the django generic ListView
+    the max number of posts per page is set to 6
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
