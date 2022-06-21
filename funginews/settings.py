e9897366dff0a0425c084243e49b3e62b86ba657
Maya-Claveau <maya.claveau@gmail.com>
2022-06-21 13:00:36 +0000

    Add landing image to home page and upload logo img

--- funginews/settings.py
+++ funginews/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -31,6 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['funginews.herokuapp.com', 'localhost']
+
 
 
 # Application definition
