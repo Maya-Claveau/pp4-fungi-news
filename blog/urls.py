7c37907655eb13c597e2abb778fa0f17cfceab2d
Maya-Claveau <maya.claveau@gmail.com>
2022-06-30 15:02:39 +0000

    Restyle the comments bg section on post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='blogpost_like'),
 ]
